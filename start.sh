95054195fb26a0e199baa0608cd68396df372cc2
valkyrie_pilot <valk@randomairborne.dev>
2021-07-16 19:25:29 +0000

    why was it python3...

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/Glaceon/Jolteon
-./venv/bin/python3 -m pip install -r requirements.txt > jolteon.log 2>&1
+./venv/bin/python -m pip install -r requirements.txt > jolteon.log 2>&1
 ./main.py --logginglevel=INFO
