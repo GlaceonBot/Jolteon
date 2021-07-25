7e6ddae2aa30ee362710b0e04c9b4de5532a0ea6
valkyrie_pilot <valk@randomairborne.dev>
2021-07-25 03:32:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -140,16 +140,13 @@ async def tag(ctx, *inputs):
             async with connection.cursor() as db:
                 for t in tags:
                     t = t.lower()
-                    if t == "help":
-                        prefix = await prefixgetter(jolteon, ctx.message)
-                        factoids.append(f"You can use the tags by using `{prefix[0]}t <tag> [@mention]`\n\n[List of tags](https://glaceon.xyz/jolteon/{ctx.guild.id}) \n\nYou can delete a tag by reacting with the 🗑️ emoji")
                     await db.execute('''SELECT tagcontent FROM tags WHERE guildid = %s AND tagname = %s''', (sid, t))
                     factoid = await db.fetchone()
-                    await db.close()
                     if factoid:
                         factoids.append(factoid[0])
-                    elif t is "help":
-                      pass
+                    elif t == "help":
+                        prefix = await prefixgetter(jolteon, ctx.message)
+                        factoids.append(f"You can use the tags by using `{prefix[0]}t <tag> [@mention]`\n\n[List of tags](https://glaceon.xyz/jolteon/{ctx.guild.id}) \n\nYou can delete a tag by reacting with the 🗑️ emoji")
                     else:
                         await ctx.send(f"tag `{t}` not found!", delete_after=15)
                         errors = True
