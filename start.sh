b651ea18e4442d1c06da6734eb7593a8de943343
valkyrie_pilot <valk@randomairborne.dev>
2021-08-26 16:28:19 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $HOME/Jolteon
-./venv/bin/python -m pip install -r requirements.txt > jolteon.log 2>&1
+./venv/bin/pip install -r requirements.txt > jolteon.log 2>&1
 ./main.py --loglevel=INFO
